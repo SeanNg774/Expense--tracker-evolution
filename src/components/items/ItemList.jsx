2c674dd9d23e63861d458d7e1ad55002ff13448b
zx-0103 <f8crjxr7bk@privaterelay.appleid.com>
2026-06-09 02:27:15 +0800

    Display enriched transaction records

--- src/components/items/ItemList.jsx
+++ src/components/items/ItemList.jsx
@@ -14,6 +14,8 @@ const ItemList = (props) => {
           title={item.title}
           income={item.income}
           amount={item.amount}
+          category={item.category}
+          date={item.date}
         />
       ))}
     </ul>
